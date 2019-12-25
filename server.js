d9d4ecfa154ba5a01d1d5d5cd158c0da24509334
mr.eshtaiwi <mr.eshtaiwi@gmail.com>
2019-12-25 15:03:10 +0200

    add darksky api for wather

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 'use strict';
 //DOTENV read our envieronment variables 
-
+require('dotenv').config();
 //express server
 //express does all the headers (envelope stuff)
 const express = require('express');
-const server = express();
-
 //who can touch our server 
 const cors = require('cors');
 const superagent = require('superagent');
-
-server.use(cors());
-require('dotenv').config();
 const PORT = process.env.PORT;
+const server = express();
+server.use(cors());
 //home route http://localhost:3000
 
 server.get('/location', locationHandler);
+server.get('/weather', weatherHandler);
+
 // server.get('/weather', weatherHandler);
 //server.use('*', notFoundHandler);
 //server.use(errorHandler);
 
 function locationHandler(request, response) {
-    let city = request.query.city;
+    let city = request.query.city;//the name city is from the query link 
+    //http://localhost:3000/location?city=amman
+    console.log(city);
     getLocation(city)
         .then(locationData => {
             console.log(locationData);
@@ -29,7 +30,6 @@ function locationHandler(request, response) {
         });
 }
 function getLocation(city) {
-    //let data = require('./data/geo.json');
     // const url = `https://maps.google.com/maps/api/geocode/json?address=${city}&key=${process.env.GOOGLE_GEOCODE_API}`
     const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_API}&city=${city}&format=json`;
     return superagent.get(url)
@@ -43,17 +43,35 @@ function Location(city, data) {
     this.latitude = data[0].lat;
     this.longitude = data[0].lon;
 }
-
+// function weatherHandlerTester(request,response) {
+//     //http://localhost:3000/weather
+//     let weatherData = require('./data/darksky.json');
+//     let weather = weatherData.daily.data;
+//     let result = weather.map(day => {
+//         return new Weather(day);
+//     });
+//     response.status(200).json(result);
+// }
 function weatherHandler(request, response) {
-    getWeather(request.query.data)
+    let city = request.query;//will read the req query
+    // console.log('the query is' , city);
+    let lat = request.query.latitude;
+    let lon = request.query.longitude;
+    // console.log('lat is ' ,lat);
+    // console.log('lon is ', lon);
+    //http://localhost:3000/weather?search_query=amman&latitude=31.9515694&longitude=35.9239625
+    //http://localhost:3000/weather?
+    // let lat = request.query;
+    // let lon = request.query;
+    // console.log('lat is ',lat);
+    // console.log('lon is',lon);
+    getWeather(lat,lon)
         .then(weatherData => {
             response.status(200).json(weatherData);
         });
 }
-function getWeather(query) {
-    //console.log(query);
-    const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${query.latitude},${query.longitude}`;
-    console.log(url);
+function getWeather(lat,lon) {
+    const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${lat},${lon}`;
     return superagent.get(url)
         .then(data => {
             let weather = data.body;
@@ -77,7 +95,7 @@ server.get('/any', (request, response) => {
 });
 
 server.use('*', (request, response) => {
-    response.status(404).send('not found');
+    response.status(404).send('Sorry, something went wrong');
 });
 
 server.use((error, request, response) => {
